ff75514f4a30d37e59cdc205032bbcec166f8bf4
amykangweb <selene6023@gmail.com>
2016-01-24 13:59:27 -0800

    Add try catch block.

--- Program.cs
+++ Program.cs
@@ -23,33 +23,40 @@ namespace FitnessFrog
                     keepGoing = false;
                 }
                 else {
-
-                    int minutes = int.Parse(entry);
-
-                    if(minutes <= 0)
+                    try
                     {
-                        Console.WriteLine(minutes + " is not an acceptable value.");
+                        int minutes = int.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+                        runningTotal = runningTotal + minutes;
+
+                        Console.WriteLine("You've exercised " + runningTotal + " minutes");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
                         continue;
                     }
-                    else if(minutes <= 10)
-                    {
-                        Console.WriteLine("Better than nothing, am I right?");
-                    }
-                    else if(minutes <= 30)
-                    {
-                        Console.WriteLine("Way to go hot stuff!");
-                    }
-                    else if(minutes <= 60)
-                    {
-                        Console.WriteLine("You must be a ninja warrior in training!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Okay, now you're just showing off!");
-                    }
-                    runningTotal = runningTotal + minutes;
-
-                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
                 }
             }
         }
