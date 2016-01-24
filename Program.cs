fc3574203f484b84acbe1eb00f3bed4ad0dcc752
amykangweb <selene6023@gmail.com>
2016-01-24 14:34:34 -0800

    Refactor code.

--- Program.cs
+++ Program.cs
@@ -10,53 +10,51 @@ namespace FitnessFrog
     {
         static void Main(string[] args)
         {
-            int runningTotal = 0;
-            bool keepGoing = true;
+            var runningTotal = 0.0;
 
-            while (keepGoing)
+            while (true)
             {
                 Console.Write("Enter how many minutes you exercised or type 'quit' to exit:  ");
 
-                string entry = Console.ReadLine();
-                if (entry == "quit")
+                var entry = Console.ReadLine();
+                if (entry.ToLower() == "quit")
                 {
-                    keepGoing = false;
+                    break;
                 }
-                else {
-                    try
-                    {
-                        int minutes = int.Parse(entry);
 
-                        if (minutes <= 0)
-                        {
-                            Console.WriteLine(minutes + " is not an acceptable value.");
-                            continue;
-                        }
-                        else if (minutes <= 10)
-                        {
-                            Console.WriteLine("Better than nothing, am I right?");
-                        }
-                        else if (minutes <= 30)
-                        {
-                            Console.WriteLine("Way to go hot stuff!");
-                        }
-                        else if (minutes <= 60)
-                        {
-                            Console.WriteLine("You must be a ninja warrior in training!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Okay, now you're just showing off!");
-                        }
-                        runningTotal = runningTotal + minutes;
+                try
+                {
+                    double minutes = double.Parse(entry);
 
-                        Console.WriteLine("You've exercised " + runningTotal + " minutes");
-                    }
-                    catch (FormatException)
+                    if (minutes <= 0)
                     {
-                        Console.WriteLine("That is not valid input.");
+                        Console.WriteLine(minutes + " is not an acceptable value.");
                         continue;
                     }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+                    runningTotal += minutes;
+
+                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
                 }
             }
         }
